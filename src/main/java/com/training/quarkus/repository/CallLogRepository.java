5d7f70d8dd61b9f06cf00875df256c13332a465a
Fernando Matias Balieiro <fernandobalieiro@gmail.com>
2022-03-06 17:05:03 +0000

    Initial commit

---
+++ src/main/java/com/training/quarkus/repository/CallLogRepository.java
@@ -0,0 +1,42 @@
+package com.training.quarkus.repository;
+
+import com.training.quarkus.model.CallLog;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class CallLogRepository {
+
+    // FIXME
+
+    private Map<String, CallLog> callLogs = new HashMap<>();
+
+    public CallLogRepository() {
+        createCallLogs();
+    }
+
+    public CallLog getCallLogByPhone(final String phone) {
+        return this.callLogs.get(phone);
+    }
+
+    private void createCallLogs() {
+        Random random = new Random();
+
+        IntStream.range(0, 10).forEach(i -> {
+                    CallLog callLog = new CallLog();
+                    String phone = "+3519999999" + i;
+                    callLog.setSource(phone);
+                    callLog.setDestination("+351" + Integer.valueOf(random.nextInt((999999999 - 888888888) + 1) + 888888888).toString());
+                    long currentTimeMillis = System.currentTimeMillis();
+                    callLog.setStartTime(new Date(currentTimeMillis));
+                    callLog.setEndTime(new Date(currentTimeMillis - 60000));
+                    this.callLogs.put(phone, callLog);
+                }
+        );
+    }
+
+    // TODO
+}
---
+++ src/main/java/com/training/quarkus/repository/CallerRepository.java
@@ -0,0 +1,38 @@
+package com.training.quarkus.repository;
+
+import com.training.quarkus.model.Caller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class CallerRepository {
+
+    // FIXME
+
+    private Map<String, Caller> callers = new HashMap<>();
+
+    public CallerRepository() {
+        createCallers();
+    }
+
+    public Caller getCallerByPhone(final String phone) {
+        return this.callers.get(phone);
+    }
+
+    private void createCallers() {
+        IntStream.range(0, 10).forEach(i -> {
+                    Caller caller = new Caller();
+                    caller.setFirstName("Caller");
+                    caller.setFirstName("" + i);
+                    caller.setBirthDayDate(new Date());
+                    String phone = "+3519999999" + i;
+                    caller.setPhone(phone);
+                    this.callers.put(phone, caller);
+                }
+        );
+    }
+
+    // TODO
+}
